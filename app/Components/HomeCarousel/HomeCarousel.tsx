aaa8847611d1902e1cdc975b34333b39c58aa496
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-06-03 07:34:50 -0700

    CONFIG

--- app/Components/HomeCarousel/HomeCarousel.tsx
+++ app/Components/HomeCarousel/HomeCarousel.tsx
@@ -11,7 +11,7 @@ const HomeCarousel = () => {
       <div id="Carousel" className='bg-[#F29AA7] w-full h-full rounded-3xl flex items-center justify-start z-50 overflow-hidden relative'>
 
         {/* LEFT DIV */}
-        <div id="left" className='flex items-center justify-center space-x-8 py-[6rem] px-10 h-full'>
+        <div id="left" className='flex items-center justify-center space-x-2 py-[6rem] px-10 h-full'>
           <Image src='/images/carousel/Left_1.png' width={500} height={500} alt='Carousel_Left_img_1' className='w-fit h-[16rem]' />
           <Image src='/images/carousel/Left_2.png' width={500} height={500} alt='Carousel_Left_img_2' className='w-fit h-[11rem]' />
         </div>
