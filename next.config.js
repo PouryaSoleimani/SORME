be65517d35ebdd175fb44ba578bcacdf40dda258
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-06-03 07:11:49 -0700

    CONFIG

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   productionBrowserSourceMaps: false,
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    if (cfg.cache && !dev) {
-      cfg.cache = Object.freeze({
-        type: 'memory',
-      })
-      cfg.cache.maxMemoryGenerations = 0
-    }
-    // Important: return the modified config
-    return config
-  },
 }
 
 export default nextConfig
