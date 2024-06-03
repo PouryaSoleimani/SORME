91858b1a635cc53847946f8428f8701ac93a0c5d
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-06-03 08:02:45 -0700

    DB

--- app/Components/ShoppingCartSideBar/ShoppingCartSideBar.tsx
+++ app/Components/ShoppingCartSideBar/ShoppingCartSideBar.tsx
@@ -6,10 +6,10 @@ import { useRecoilState } from 'recoil'
 const ShoppingCartSideBar = () => {
   const [BAG, setBAG] = useRecoilState(SHOPPINGCART)
   return (
-    <div className='w-1/2 h-screen bg-zinc-200 border-l-8 border-pink-400'>
+    <div className='w-1/3 h-screen bg-zinc-200 border-l-8 border-pink-400'>
       <h1>Your Cart Reviews :
-        <h2>Items Count : {BAG.length}</h2>
-        <h2>Total Price : </h2>
+        <span>Items Count : {BAG.length}</span>
+        <span>Total Price : </span>
       </h1>
     </div>
   )
