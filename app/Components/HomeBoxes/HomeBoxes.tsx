7be3f456edc226930bf90c34471a4c73dbda7e24
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-06-03 07:23:24 -0700

    CONFIG

--- app/Components/HomeBoxes/HomeBoxes.tsx
+++ app/Components/HomeBoxes/HomeBoxes.tsx
@@ -12,11 +12,11 @@ import 'aos/dist/aos.css';
 const HomeBoxes = () => {
   AOS.init()
   return (
-    <div data-aos="zoom-in-up" data-aos-duration="700" data-aos-easing="linear" className='flex items-center flex-shrink justify-around py-2 px-16'>
-      <Image as={NextImage} isZoomed src='/images/Home4Boxes/Box1.png' width={500} height={500} alt='Home-Boxes_Box1' className='rounded-2xl w-[18.5rem] h-full' />
-      <Image as={NextImage} isZoomed src='/images/Home4Boxes/Box2.png' width={500} height={500} alt='Home-Boxes_Box2' className='rounded-2xl w-[18.5rem] h-full' />
-      <Image as={NextImage} isZoomed src='/images/Home4Boxes/Box3.png' width={500} height={500} alt='Home-Boxes_Box3' className='rounded-2xl w-[18.5rem] h-full' />
-      <Image as={NextImage} isZoomed src='/images/Home4Boxes/Box4.png' width={500} height={500} alt='Home-Boxes_Box4' className='rounded-2xl w-[18.5rem] h-full' />
+    <div data-aos="zoom-in-up" data-aos-duration="700" data-aos-easing="linear" className='flex items-center justify-around space-x-16 flex-grow py-10 px-24'>
+      <Image as={NextImage} isZoomed src='/images/Home4Boxes/Box1.png' width={500} height={500} alt='Home-Boxes_Box1' className='rounded-2xl' />
+      <Image as={NextImage} isZoomed src='/images/Home4Boxes/Box2.png' width={500} height={500} alt='Home-Boxes_Box2' className='rounded-2xl ' />
+      <Image as={NextImage} isZoomed src='/images/Home4Boxes/Box3.png' width={500} height={500} alt='Home-Boxes_Box3' className='rounded-2xl ' />
+      <Image as={NextImage} isZoomed src='/images/Home4Boxes/Box4.png' width={500} height={500} alt='Home-Boxes_Box4' className='rounded-2xl ' />
     </div >
   )
 }
