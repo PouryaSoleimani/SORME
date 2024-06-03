e226a9e4ff7c3273549cfbd78c090990b63edea8
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-06-03 08:47:59 -0700

    SHOP

--- app/Components/ShoppingCartSideBar/ShoppingCartSideBar.tsx
+++ app/Components/ShoppingCartSideBar/ShoppingCartSideBar.tsx
@@ -1,16 +1,36 @@
 "use client"
 import { SHOPPINGCART } from '@/app/Recoil/atoms'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
+type SingleProductType = { id: number, title: string, img: string, rating: number, views: number, price: number, seller: string, brand: string, off: boolean, offPercent: number, count: number }
+
+
 
 const ShoppingCartSideBar = () => {
   const [BAG, setBAG] = useRecoilState(SHOPPINGCART)
+  const [totalCheck, setTotalCheck] = useState(0)
+  const prices = BAG.map((item: SingleProductType) => item.price)
+
+  useEffect(() => {
+    if (prices.length) {
+      let sum = prices.reduce((a: number, b: number) => a + b)
+      setTotalCheck(sum)
+    }
+  }, [])
+
+  function emptyBagHandler() { setBAG([]) }
+
   return (
-    <div className='w-1/3 h-screen bg-zinc-200 border-l-8 border-pink-400'>
-      <h1>Your Cart Reviews :
-        <span>Items Count : {BAG.length}</span>
-        <span>Total Price : </span>
-      </h1>
+    <div className='w-2/5 h-screen bg-zinc-200 border-l-8 border-pink-300 px-10'>
+      <h1 className='text-3xl font-bold text-center py-2 underline decoration-4 decoration-pink-300'>Your Cart Reviews :</h1>
+      <span className='text-xl font-bold mt-16 mb-4'>Items Count : {BAG.length}</span>
+      <p className='text-xl font-bold text-blue-700 my-4'>Total Item Prices : {totalCheck}$ </p>
+      <p className='text-xl font-bold my-4'>Tax : 10%</p>
+      <p className='text-3xl font-bold text-emerald-700 my-4 bg-lime-300 px-4 py-2 rounded-xl'> Total Check : {totalCheck * 110 / 100}</p>
+      <div className='flex flex-col space-y-6 mt-10'>
+        <button className='px-2 py-4 rounded-xl bg-pink-300 w-[20rem] font-bold text-xl hover:bg-pink-800 hover:text-white duration-200'>Go to CheckOut</button>
+        <button onClick={emptyBagHandler} className='px-2 py-4 rounded-xl bg-red-500 w-[20rem] font-bold text-xl hover:bg-red-800 hover:text-white duration-200'>Empty ShoppingCart</button>
+      </div>
     </div>
   )
 }
