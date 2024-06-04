0983c40beb0b0511ebad0169d36accc53332f835
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-06-04 13:07:38 -0700

    REGSITER

--- app/(Login_Resigster)/(CUSTOMER)/register/page.tsx
+++ app/(Login_Resigster)/(CUSTOMER)/register/page.tsx
@@ -9,27 +9,34 @@ import { FiEye } from "react-icons/fi";
 import { FiEyeOff } from "react-icons/fi";
 import toast, { Toaster } from 'react-hot-toast';
 import { useRecoilState } from "recoil";
-import { USERINFOS, isLoggedIn } from "@/app/Recoil/atoms";
+import { TOKEN, USERINFOS, isLoggedIn } from "@/app/Recoil/atoms";
 import axios from 'axios'
+import { useRouter } from "next/navigation";
 
 
 type Inputs = { username: string, email: string, password: string }
 
 const RegisterPage = ({ }) => {
   const notify = () => toast.success('Signed Up Successfully', { style: { border: '3px solid #F29AA7', padding: '10px', color: 'black', fontWeight: 'bold', fontSize: '16px', borderRadius: "10px" } })
+  const notify2 = () => toast.error('There Was a Problem , Try Again', { style: { border: '3px solid #F29AA7', padding: '10px', color: 'black', fontWeight: 'bold', fontSize: '16px', borderRadius: "10px" } })
+  const router = useRouter()
+  //STATES
   const [ISLOGGEDIN, setISLOGGEDIN] = useRecoilState(isLoggedIn)
   const [userInfos, setUserInfos] = useRecoilState(USERINFOS)
-
+  const [token, setToken] = useRecoilState(TOKEN)
   //^YUP
   const schema = yup.object().shape({
     username: yup.string().required("Name is Required"),
     email: yup.string().email("Invalid Email").required("Email is Required").min(10, 'Email Must be Longer').max(30, "Email Must be Max 30 Chars. "),
-    password: yup.string().required('Password is Required').min(10 , 'Password Must be Longer')
+    password: yup.string().required('Password is Required').min(10, 'Password Must be at least 10 words')
   }).required();
 
   //&REACT-HOOK-FORM
   const { register, handleSubmit, formState: { errors }, reset } = useForm<Inputs>({ resolver: yupResolver(schema) })
-  const sendData = (data: {}) => { axios.post('http://localhost:3000/users', data).then(response => console.log(response)).catch(error => console.log(error)) }
+  const sendData = (data: {}) => {
+    axios.post('http://localhost:3000/users', data)
+      .then((response) => { console.log(response); setToken(response.data.accessToken) }).catch(error => { console.log(error); notify2() })
+  }
   //*SUBMIT HANDLER
   const onSubmit: SubmitHandler<Inputs> = (data, event) => {
     event?.preventDefault()
@@ -38,6 +45,7 @@ const RegisterPage = ({ }) => {
     notify();
     reset();
     setISLOGGEDIN(true)
+    setTimeout(() => { router.push("/login") }, 1000);
   }
 
 
@@ -67,21 +75,21 @@ const RegisterPage = ({ }) => {
             {/* USERNAME */}
             <span className='flex flex-col'>
               <label htmlFor="username" className='absolute -translate-y-4 text-xl font-bold z-30 translate-x-4 text-white bg-[#F29AA7] px-2 '>Username</label>
-              <input type="text" placeholder='Sourosh Rasouli' {...register("username")} className='px-5 py-5 z-0 border-4 placeholder:text-xl relative left-0 placeholder:text-white border-white bg-transparent rounded-lg w-full text-3xl text-white font-semibold' />
+              <input type="text" placeholder='Sourosh Rasouli' {...register("username")} className='px-5 py-5 z-0 border-4 placeholder:text-xl relative left-0 placeholder:text-white/50 border-white bg-transparent rounded-lg w-full text-3xl text-white font-semibold' />
               <FaUser className="text-white w-10 h-12 absolute translate-y-3 translate-x-[26.5rem]" />
               {errors.username && <p className='text-red-700 font-semibold text-2xl'>{errors.username.message}</p>}
             </span>
             {/* EMAIL ADDRESS */}
             <span className='flex flex-col'>
               <label htmlFor="email" className='absolute -translate-y-4 text-xl font-bold z-30 translate-x-4 text-white bg-[#F29AA7] px-2 '>Email Address</label>
-              <input type="text" placeholder='bndfjbf@gmailcom' {...register("email")} className='px-5 py-5 z-0 border-4 placeholder:text-xl relative left-0 placeholder:text-white border-white bg-transparent rounded-lg w-full text-3xl text-white font-semibold' />
+              <input type="text" placeholder='bndfjbf@gmailcom' {...register("email")} className='px-5 py-5 z-0 border-4 placeholder:text-xl relative left-0 placeholder:text-white/50 border-white bg-transparent rounded-lg w-full text-3xl text-white font-semibold' />
               <FaUser className="text-white w-10 h-12 absolute translate-y-3 translate-x-[26.5rem]" />
               {errors.email && <p className='text-red-700 font-semibold text-2xl'>{errors.email.message}</p>}
             </span>
             {/* PASSWORD */}
             <span className='flex flex-col'>
               <label htmlFor="password" className='absolute -translate-y-4 text-xl font-bold z-30 translate-x-4 text-white bg-[#F29AA7] px-2 '>Password</label>
-              <input type={isVisible ? "text" : "password"} placeholder='***********' {...register("password")} className='px-5 py-5 z-0 border-4 placeholder:text-xl relative left-0 placeholder:text-white border-white bg-transparent rounded-lg w-full text-3xl text-white font-semibold' />
+              <input type={isVisible ? "text" : "password"} placeholder='***********' {...register("password")} className='px-5 py-5 z-0 border-4 placeholder:text-xl relative left-0 placeholder:text-white/50 border-white bg-transparent rounded-lg w-full text-3xl text-white font-semibold' />
               <span onClick={toggleVisibility} className="text-white text-5xl w-10 h-12 absolute translate-y-[14px] translate-x-[26.2rem] cursor-pointer" >
                 {isVisible ? (
                   <FiEye />
