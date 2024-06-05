b49cbd7312daec71b81dc1d2371bb4003ad0f45b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-06-04 17:46:34 -0700

    LOGIN

--- app/Components/ShoppingCartSideBar/ShoppingCartSideBar.tsx
+++ app/Components/ShoppingCartSideBar/ShoppingCartSideBar.tsx
@@ -25,13 +25,13 @@ const ShoppingCartSideBar = () => {
   return (
     <div className='w-2/5 h-screen bg-zinc-200 border-l-8 border-pink-300 px-10'>
       <h1 className='text-3xl font-bold text-center py-2 underline decoration-4 decoration-pink-300'>Your Cart Reviews :</h1>
-      <span className='text-xl font-bold mt-16 mb-4'>Items Count : {BAG?.length}</span>
+      <span className='text-xl font-bold mt-24 mb-4'>Items Count : {BAG?.length}</span>
       <p className='text-xl font-bold text-blue-700 my-4'>Total Item Prices : ${totalCheck}</p>
-      <p className='text-xl font-bold my-4'>Tax : 10%</p>
-      <p className='text-3xl font-bold text-emerald-700 my-4 bg-lime-300 px-4 py-2 rounded-xl'> Total Check : ${totalCheck * 110 / 100}</p>
-      <div className='flex flex-col space-y-6 mt-10'>
-        <button className='px-2 py-4 rounded-xl bg-pink-300 w-[20rem] font-bold text-xl hover:bg-pink-800 hover:text-white duration-200'>Go to CheckOut</button>
-        <button onClick={emptyBagHandler} className='px-2 py-4 rounded-xl bg-red-500 w-[20rem] font-bold text-xl hover:bg-red-800 hover:text-white duration-200'>Empty ShoppingCart</button>
+      <p className='text-xl font-bold my-4 text-zinc-500'>Tax : 10%</p>
+      <p className='text-3xl font-bold text-emerald-700 my-4 bg-emerald-200 p-6 border-b-4 border-emerald-700 rounded-xl'> Total Check : ${totalCheck * 110 / 100}</p>
+      <div className='flex flex-col items-center space-y-6 mt-10'>
+        <button className='px-2 py-4 rounded-xl bg-blue-300 w-[20rem] font-bold text-xl hover:bg-blue-600 hover:text-white duration-300'>Go to CheckOut</button>
+        <button onClick={emptyBagHandler} className='px-2 py-4 rounded-xl bg-red-300 w-[20rem] font-bold text-xl hover:bg-red-600 hover:text-white duration-300'>Empty ShoppingCart</button>
       </div>
     </div>
   )
