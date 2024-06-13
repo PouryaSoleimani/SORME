8136de738fe061f40e40315086f3d4b3e35220b4
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-06-13 06:44:57 -0700

    DEBUGGING

--- app/Components/HomeBoxes/HomeBoxes.tsx
+++ app/Components/HomeBoxes/HomeBoxes.tsx
@@ -1,8 +1,7 @@
 "use client"
 //^ HOME BOXES =======================================================================================================================================================
 import { Image } from '@nextui-org/image';
-import NextImage from 'next/image'
-import React from 'react'
+import React, { useEffect } from 'react'
 import 'animate.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -10,13 +9,14 @@ import 'aos/dist/aos.css';
 
 
 const HomeBoxes = () => {
-  AOS.init()
+  useEffect(() => { AOS.init() }, [])
+
   return (
     <div data-aos="zoom-in-up" data-aos-duration="700" data-aos-easing="linear" className='flex items-center justify-around space-x-16 flex-grow py-10 px-24'>
-      <Image as={NextImage} isZoomed src='/images/Home4Boxes/Box1.png' width={500} height={500} alt='Home-Boxes_Box1' className='rounded-2xl' />
-      <Image as={NextImage} isZoomed src='/images/Home4Boxes/Box2.png' width={500} height={500} alt='Home-Boxes_Box2' className='rounded-2xl ' />
-      <Image as={NextImage} isZoomed src='/images/Home4Boxes/Box3.png' width={500} height={500} alt='Home-Boxes_Box3' className='rounded-2xl ' />
-      <Image as={NextImage} isZoomed src='/images/Home4Boxes/Box4.png' width={500} height={500} alt='Home-Boxes_Box4' className='rounded-2xl ' />
+      <Image isZoomed src='/images/Home4Boxes/Box1.png' width={500} height={500} alt='Home-Boxes_Box1' className='rounded-2xl' />
+      <Image isZoomed src='/images/Home4Boxes/Box2.png' width={500} height={500} alt='Home-Boxes_Box2' className='rounded-2xl ' />
+      <Image isZoomed src='/images/Home4Boxes/Box3.png' width={500} height={500} alt='Home-Boxes_Box3' className='rounded-2xl ' />
+      <Image isZoomed src='/images/Home4Boxes/Box4.png' width={500} height={500} alt='Home-Boxes_Box4' className='rounded-2xl ' />
     </div >
   )
 }
