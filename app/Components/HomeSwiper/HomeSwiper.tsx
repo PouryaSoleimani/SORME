71270b103045a5c169b8c44156b02245c4fbc474
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-06-13 12:45:19 -0700

    SWIPER

--- app/Components/HomeSwiper/HomeSwiper.tsx
+++ app/Components/HomeSwiper/HomeSwiper.tsx
@@ -13,10 +13,10 @@ import { Image } from '@nextui-org/react';
 const HomeSwiper = () => {
   return (
     <div className='py-6 px-4 w-screen'>
-      <Swiper spaceBetween={30} rewind={true} centeredSlides={true} grabCursor={true} autoplay={{ delay: 3000, disableOnInteraction: false }} navigation={true} loop={true} modules={[Autoplay, Navigation]} className="mySwiper">
-        <SwiperSlide><Image src='/images/swiper/SwiperSlide1.png' width={1200} height={800} className='rounded-3xl w-screen' /></SwiperSlide>
-        <SwiperSlide><Image src='/images/swiper/SwiperSlide2.png' width={1200} height={800} className='rounded-3xl w-screen' /></SwiperSlide>
-        <SwiperSlide><Image src='/images/swiper/SwiperSlide3.png' width={1200} height={800} className='rounded-3xl w-screen' /></SwiperSlide>
+      <Swiper spaceBetween={30} rewind={true} centeredSlides={true} grabCursor={true} autoplay={{ delay: 3000, disableOnInteraction: false }} navigation={true} loop={true} modules={[Autoplay, Navigation]} className="mySwiper w-screen flex items-center justify-center">
+        <SwiperSlide><Image src='/images/swiper/SwiperSlide1.png' width={1700} height={1200} className='rounded-3xl' /></SwiperSlide>
+        <SwiperSlide><Image src='/images/swiper/SwiperSlide2.png' width={1700} height={1200} className='rounded-3xl' /></SwiperSlide>
+        <SwiperSlide><Image src='/images/swiper/SwiperSlide3.png' width={1700} height={1200} className='rounded-3xl' /></SwiperSlide>
       </Swiper>
     </div>
   )
