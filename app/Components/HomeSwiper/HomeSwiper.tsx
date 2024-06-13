abfbd1556f159a7ca5f75f983cabf8dc0aa4fe35
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-06-13 10:55:25 -0700

    SWIPER

--- app/Components/HomeSwiper/HomeSwiper.tsx
+++ app/Components/HomeSwiper/HomeSwiper.tsx
@@ -13,7 +13,7 @@ import { Image } from '@nextui-org/react';
 const HomeSwiper = () => {
   return (
     <div className='py-6 px-4 w-screen'>
-      <Swiper spaceBetween={30} centeredSlides={true} autoplay={{ delay: 3000, disableOnInteraction: false }} navigation={true} loop={true} modules={[Autoplay, Navigation]} className="mySwiper">
+      <Swiper spaceBetween={30} rewind={true} centeredSlides={true} autoplay={{ delay: 3000, disableOnInteraction: false }} navigation={true} loop={true} modules={[Autoplay, Navigation]} className="mySwiper">
         <SwiperSlide><Image src='/images/swiper/SwiperSlide1.png' width={1200} height={800} className='rounded-3xl w-screen' /></SwiperSlide>
         <SwiperSlide><Image src='/images/swiper/SwiperSlide2.png' width={1200} height={800} className='rounded-3xl w-screen' /></SwiperSlide>
         <SwiperSlide><Image src='/images/swiper/SwiperSlide3.png' width={1200} height={800} className='rounded-3xl w-screen' /></SwiperSlide>
