37ec8ec55070011e9010318496adca4a0486bddd
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-06-14 02:07:26 -0700

    MIIDLEWARES

--- app/singleproduct/[id]/page.tsx
+++ app/singleproduct/[id]/page.tsx
@@ -4,6 +4,8 @@ import Footer from '@/app/Components/Footer/Footer'
 import Header from '@/app/Components/Header/Header'
 import SingleProduct from '@/app/Components/SingleProduct/SingleProduct'
 
+
+
 type ParamsType = { params: { id: string } }
 
 
