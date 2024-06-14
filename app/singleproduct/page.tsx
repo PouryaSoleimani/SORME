b3900e4c728ab24c67a809f7795e97d6bf8f35c6
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-06-14 09:00:56 -0700

    DEBUGGING

--- app/singleproduct/page.tsx
+++ app/singleproduct/page.tsx
@@ -3,21 +3,21 @@ import React from 'react'
 import Header from '../Components/Header/Header'
 import BreadCrumb from '../Components/BreadCrumb/BreadCrumb'
 import Footer from '../Components/Footer/Footer'
-import SingleProduct from '../Components/SingleProduct/SingleProductComponent'
+import SingleProductComponent from '../Components/SingleProduct/SingleProductComponent'
 
 type ParamsType = { params: { id: string } }
 
 const SingleProductPage = async (params: ParamsType) => {
 
   const productID = params.params.id
-  const request = await fetch(`http://localhost:3000/products/${productID}`, { cache: 'default' })
+  const request = await fetch(`http://localhost:3000/products/${productID}`, { cache: 'no-store' })
   const response = await request.json()
 
   return (
     <section suppressHydrationWarning className='overflow-hidden'>
       <Header />
       <BreadCrumb />
-      <SingleProduct {...response} />
+      <SingleProductComponent {...response} />
       <Footer />
     </section>
   )
