efd14ea2fd25c715193f506850ea799169666112
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-06-15 04:19:29 -0700

    DEBUGGING

--- app/singleproduct/[id]/page.tsx
+++ app/singleproduct/[id]/page.tsx
@@ -1,9 +1,12 @@
 //^ SINGLE PRODUCT PAGE 2 =================================================================================================================================================
+import dynamic from 'next/dynamic'
 import BreadCrumb from '@/app/Components/BreadCrumb/BreadCrumb'
 import Footer from './../../Components/Footer/Footer'
 import Header from '@/app/Components/Header/Header'
 import SingleProductComponent from './../../Components/SingleProduct/SingleProductComponent'
 import 'aos/dist/aos.css';
+
+const SingleProductDynamic = dynamic(() => import('./../../Components/SingleProduct/SingleProductComponent'), { ssr: false })
 
 type ParamsType = { params: { id: string } }
 
@@ -14,10 +17,10 @@ const SingleProductPage2 = async (params: ParamsType) => {
   const response = await request.json()
 
   return (
-    <section suppressHydrationWarning className='overflow-hidden'>
+    <section className='overflow-hidden'>
       <Header />
       <BreadCrumb />
-      <SingleProductComponent {...response} />
+      <SingleProductDynamic {...response} />
       <Footer />
     </section>
 
