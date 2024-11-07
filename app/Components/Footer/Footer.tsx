a2636d9cff93504103b02a7044570f944e6f2573
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-11-07 20:35:12 +0330

    RESPONSIVE

--- app/Components/Footer/Footer.tsx
+++ app/Components/Footer/Footer.tsx
@@ -24,14 +24,14 @@ const Footer = () => {
 
       </div>
 
-      <div id="middle" className='flex items-center justify-between w-full py-10'>
+      <div id="middle" className='flex items-center justify-between w-full py-4'>
         <div id="middleLeft" className='flex items-center justify-start space-x-8 w-1/3'>
           <img src="/images/footer/middleLeft_1.png" alt="E-Namad Logo" className='w-24 h-24' />
           <img src="/images/footer/middleLeft_2.png" alt="Organization Logo" className='w-24 h-24' />
           <img src="/images/footer/middleLeft_3.png" alt="Samandehi Logo" className='w-24 h-24' />
         </div>
 
-        <div id="middleRight" className='flex items-center justify-between w-2/3 px-12'>
+        <div id="middleRight" className='flex items-center justify-between w-2/3 pl-24 -translate-y-10 tracking-tight font-extralight'>
           <h2 className='text-3xl leading-loose text-zinc-500 font-semibold'>info@sorme.com</h2>
           <h2 className='text-3xl leading-loose text-zinc-500 font-semibold'>02177111258</h2>
         </div>
@@ -39,6 +39,7 @@ const Footer = () => {
 
       <div id="buttom" className='text-center text-sm py-2'>
         <h3 className='text-zinc-900'>All Material and Intellectual Rights of this Website are Reserved for "Sormeh" ©2020-2024</h3>
+        <h3 className='text-zinc-900'>Created Using React.js & Next.js by <Link href="https://github.com/PouryaSoleimani" className='text-blue-800 underline font-bold'>Pourya Soleimani</Link></h3>
       </div>
     </div>
   )
