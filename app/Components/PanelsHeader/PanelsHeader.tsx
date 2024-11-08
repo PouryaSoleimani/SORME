5a79c675b9e38b49b053c6d172f251a5b887a1d2
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-11-08 13:17:27 +0330

    REFACTOR

--- app/Components/PanelsHeader/PanelsHeader.tsx
+++ app/Components/PanelsHeader/PanelsHeader.tsx
@@ -1,12 +1,18 @@
-//^ PANELS HEADER
-// "use client"
+//^ PANELS HEADER ========================================================================================================================================================================================
+"use client"
 import Image from 'next/image'
 import React from 'react'
 import Link from 'next/link';
 import { PiPackageLight } from "react-icons/pi";
 import 'animate.css';
+import { useRecoilState } from 'recoil';
+import { isLoggedIn, USERINFOS } from '@/Recoil/atoms';
+
 
 const PanelsHeader: React.FC = () => {
+  const [ISLOGGEDIN, setISLOGGEDIN] = useRecoilState(isLoggedIn)
+  const [userInfos, setUserInfos] = useRecoilState(USERINFOS)
+
   return (
     <section className='w-screen h-fit px-20 py-2 flex items-center justify-between overflow-hidden animate__animated z-30 shadow-lg shadow-zinc-300 bg-zinc-50 absolute top-0 left-0'>
 
@@ -20,9 +26,9 @@ const PanelsHeader: React.FC = () => {
       {/* RIGHT */}
       <div id="right" className='flex items-center justify-center before:h-fit divide-x divide-black'>
 
-        <Link href='/register' className='px-4'>
-          <span id='SignUpButton' className='border border-black rounded-md flex p-1 space-x-1 items-center justify-center hover:bg-[#F29AA7]  duration-300'>
-            <p>Sign In / SignUp</p>
+        <Link href={`${!ISLOGGEDIN ? '/register' : '/customer-panel'}`} suppressHydrationWarning className='px-4'>
+          <span id='SignUpButton' className={`border border-black rounded-md flex py-1 px-2 space-x-1 items-center justify-center hover:bg-[#F29AA7]  hover:text-white  duration-300 ${ISLOGGEDIN ? `border-2 border-green-600 bg-green-100 hover:bg-green-900` : null}`}>
+            <p>{!ISLOGGEDIN ? "Sign In / SignUp" : userInfos?.username} </p>
             <img src="/images/icons/HeaderUser.png" className='w-5 h-5' />
           </span>
         </Link>
